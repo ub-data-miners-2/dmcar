82b75a8ca62a48e3c0854c83ccb01af00c7ae8c3
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2019-04-25 15:51:09 -0400

    new file to manage car

---
+++ logic.py
@@ -0,0 +1,24 @@
+import numpy as np
+from keras.models import load_model
+
+MODEL_PATH = "traffic_sign.model"
+DICT = ["other", "speed", "stop", "yield"]
+
+class Logic:
+    def __init__(self):
+        self.model = load_model(MODEL_PATH)
+
+    def identify(self, image):
+        predictions = self.model.predict(image)[0]
+        answer = max(predictions)
+        index = np.where(predictions == answer)
+        return [ DICT[index[0][0]], answer]
+
+    def action_to_take(self, image):
+        action = "move"
+        sign, confidence = self.identify(image)
+        if sign == "stop":
+            action = "stop"
+        elif sign == "yield":
+            action = "slow"
+        return action
